c48a2020cb765317ab41bf5247ed298273392b91
marcusmayorga <mayorga.ml@gmail.com>
2021-05-07 16:03:13 -0700

    completed assessment

--- code-challenges.js
+++ code-challenges.js
@@ -9,10 +9,35 @@
 // --------------------1) Create a function that takes in a number and determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Fahrenheit.
 // Use the test variables provided below. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp1 = 35
-var temp2 = 350
-var temp3 = 212
+// var temp1 = 35;
+// var temp2 = 350;
+// var temp3 = 212;
 
+        // Make a function that takes in a parameter for the number
+        // const testTemp = (temp) => {
+        //     // Make conditions to test the temp
+
+        //     // First one is equal or below 35
+        //     if (temp <= 35) {
+        //         // Return if below or equal to 35
+        //         return `${temp} is below boiling point`;
+        //     } 
+        //     // Check if equal to 212
+        //     else if (temp === 212) {
+        //         // Return if equal to 212
+        //         return `${temp} is at boiling point`;
+        //     }
+        //     // Check if above boiling point
+        //     else if (temp >= 212){
+        //         // Return if above or equal to 212
+        //         return `${temp} is above boiling point`;
+        //     } else {
+        //         return `Something went wrong :-( `;
+        //     }
+        // }
+        // console.log(testTemp(temp1));
+        // console.log(testTemp(temp2));
+        // console.log(testTemp(temp3));
 
 
 
@@ -20,27 +45,73 @@ var temp3 = 212
 // --------------------2) Create a function that takes in an array of numbers and returns an array with each number multiplied by 5.
 // Use the test variable provided below. Expected output: [15, 35, 0, 30, -45]
 
-var myNumbers1 = [3, 7, 0, 6, -9]
+// var myNumbers1 = [3, 7, 0, 6, -9]
 
+        // // Make a function that takes an array of numbers as param
+        // const multiByFive = (arr) => {
+        //     // I need a new array to hold for output
+        //     let numsTimesFive = [];
 
+        //     //Loop through the array to get the numbers
+        //     for(let i = 0;i < arr.length; i++) {
+        //         // Multiply numbers in array by 5
+        //         // Push to new array
+        //         numsTimesFive.push(arr[i] * 5);
+        //     }
+        //     // Return new array with new array
+        //     return numsTimesFive;
+        // }
 
+        // console.log(multiByFive(myNumbers1));
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns an array containing only the odd numbers.
 // Use the test variable provided below. Expected output: [-7, 9, 13]
 
-var myNumbers2 = [8, -7, 0, 6, 2, 9, 13]
+// var myNumbers2 = [8, -7, 0, 6, 2, 9, 13]
 
+        // // Make function with param to test odd numbers
+        // const onlyFansOfOdds = (arr) => {
+        //     // Make an empty array for final output
+        //     let onlyOddNums = [];
+        //     // Go through array. Yay!
+        //     for(let i = 0; i < arr.length; i++) {
+        //         // Test if each array number is odd. % by 2 with no 0
+        //         if (arr[i] % 2 !== 0) {
+        //             onlyOddNums.push(arr[i]);
+        //         }
+        //     }
+        //     // Return the new final array
+        //     return onlyOddNums;
+        // }
 
+        // console.log(onlyFansOfOdds(myNumbers2));
 
 
 
 // --------------------4) Create a function that removes all the vowels from a string.
 // Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
 
-var stringWithVowels1 = "HeyThereLearnStudent"
-var stringWithVowels2 = "ILoveJavaScript"
+// var stringWithVowels1 = "HeyThereLearnStudent"
+// var stringWithVowels2 = "ILoveJavaScript"
 
+        // // Make function with param to hold string and remove vowels
+        // const removeVowels = (str) => {
+        //     // create empty var to hold final string
+        //     let noVowels = '';
+        //     //test if vowels are in string
+        //     for (let i = 0; i < str.length; i++) {
+        //         // if the letter is a vowel, skip it
+        //         // NOTE: input param test for condition changed to lowercase to grab all letters
+        //         if (str[i].toLowerCase() != "a" && str[i].toLowerCase() != "e" && str[i].toLowerCase() != "i" && str[i].toLowerCase() != "o" && str[i].toLowerCase() != "u") {
+        //             //add letter to noVowels to make new string
+        //             noVowels += str[i];
+        //         };   
+        //     }
+        //     return noVowels;
+        // }
+        // console.log(removeVowels(stringWithVowels1));
+        // console.log(removeVowels(stringWithVowels2));
 
 
 
@@ -52,3 +123,34 @@ var stringWithVowels2 = "ILoveJavaScript"
 var refactorTester1 = true
 var refactorTester2 = 42
 var refactorTester3 = "IAmACodingMaster"
+
+        // Make function with param to hold string and remove vowels
+        const removeVowels = (str) => {
+            // create empty var to hold final string
+            let noVowels = '';
+            // First I should test if the param is even a string
+            if(typeof str === "string") {
+                // if it's true run the OG stuff:
+
+                //test if vowels are in string
+                for (let i = 0; i < str.length; i++) {
+                
+                // if the letter is a vowel, skip it
+                // NOTE: input param test for condition changed to lowercase to grab all letters
+                    if (str[i].toLowerCase() != "a" && str[i].toLowerCase() != "e" && str[i].toLowerCase() != "i" && str[i].toLowerCase() != "o" && str[i].toLowerCase() != "u") {
+                    //add letter to noVowels to make new string
+                    noVowels += str[i];
+                    };   
+                }
+            } else if (typeof str === "boolean") {
+                return `${str} is not a string`;
+            } else if (typeof str === "number") {
+                return `${str} is not a string`;
+            } else {
+                return `Some crazy stuff happened!`;
+            } 
+            return noVowels;
+        }
+        console.log(removeVowels(refactorTester1));
+        console.log(removeVowels(refactorTester2));
+        console.log(removeVowels(refactorTester3));
--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,8 @@
 var cohort = "Bravo 2021"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The ouput to the console should be 10 (includes space);
+// b) Verify and explain: Answer is 10 because it's using the method to find the length of the variable
 
 
 // --------------------2) What will this log?
@@ -19,8 +19,8 @@ var cohort = "Bravo 2021"
 var greeting = "Hello World!"
 // console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The output will be the second "l" because it's in index 3
+// b) Verify and explain: It's "l" because it's in the 3rd index (4th letter)
 
 
 // --------------------3) What will this log?
@@ -29,8 +29,8 @@ var languages = ["JavaScript", "Ruby", "Python", "C++"]
 var index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The answer is Ruby
+// b) Verify and explain: The answer is Ruby because the index variable is '1' which is the index of that word in the languaes array.
 
 
 // --------------------4) What will this log?
@@ -38,8 +38,8 @@ var index = 1
 var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ["SATURDAY","SUNDAY"]
+// b) Verify and explain: AH! Not Correct. The array values can't be changed with toUpperCase alone without looping through each value in the array
 
 
 // --------------------5) What will this log?
@@ -47,5 +47,5 @@ var weekendDays = ["saturday", "sunday"]
 var dataTypes = ["number", "string", "Boolean"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "array" 3
+// b) Verify and explain: Not Correct. The typeof will return number because dataTypes with the length property is returning "3" which is a number
